f772043548bf34f8ad324d4ad55bca6f6cb43780
Tahir Hashmi <tahir.hashmi@tokopedia.com>
2017-10-28 12:17:48 +0700

    Revised interface. Added documentation

--- smartcb.go
+++ smartcb.go
@@ -1,15 +1,3 @@
-// packate smartcb implements a `TripFunc` for github.com/rubyist/circuitbreaker
-// that automatically adjusts the tripping error threshold based on abnormal
-// increase in error rate.
-//
-// The circuit breaker starts off with a learning phase for understanding the error
-// profile of the wrapped command and then adjusts its tripping threshold for error
-// rate based on what it has learned.
-//
-// The error threshold is calculated as an exponential weighted moving average which
-// smoothens out jitter and can detect rapid changes in error rate, allowing for the
-// circuit to trip fast in case of a rapid degradation of the wrapped command.
-
 package smartcb
 
 import (
@@ -19,56 +7,169 @@ import (
 	"github.com/rubyist/circuitbreaker"
 )
 
-const minFail = 0.001
-const maxFail = 0.4
-const learningCycles = 10    // Size of learning window relative to decision window
-const relearningCycles = 100 // No. of decision cycles after which to re-trigger learning
+// Policies for configuring the circuit breaker's decision making
+// If you must, experiment with the parameters one at a time. All parameters
+// are required to be > 0
+type Policies struct {
+	// Circuit breaker trips when the error rate hits FailMultiplier times of the learned rate
+	// You could adjust this based on your error rate profile
+	FailMultiplier float64
+	// Absolute highest failure rate above which the breaker must open
+	// Default is 0.4 (40%). You should definitely review this number
+	MaxFail float64
 
-func New(QPS int) *circuit.Breaker {
-	decisionWindow := time.Millisecond * time.Duration(1000000.0/float64(QPS))
-	if decisionWindow <= 0 {
-		panic("smartcb.NewSmartTripper: decisionWindow should be a valid duration")
+	// Number of "decision windows" used for learning
+	LearningWindowX float64
+	// Number of "decision windows" after which learning is restarted.
+	//
+	// This setting must be greater than LearningWindowX otherwise the breaker
+	// would be in a perpetual learning state
+	ReLearningWindowX float64
+	// Smoothing factor for error rate learning. Higher numbers reduce jitter but cause more lag
+	EWMADecayFactor float64
+	// Minimum number of error/success incidents required for minimum decision making
+	SamplesPerWindow int64
+}
+
+var defaults = Policies{
+	MaxFail:           0.4,
+	LearningWindowX:   10.0,
+	ReLearningWindowX: 100.0,
+	EWMADecayFactor:   10.0,
+
+	FailMultiplier:   3.0,
+	SamplesPerWindow: 100,
+}
+
+func min(l, r float64) float64 {
+	if l < r {
+		return l
 	}
+	return r
+}
 
-	options := &circuit.Options{
-		WindowTime: decisionWindow,
+// Circuit Breaker's Learning State
+type State int
+
+const (
+	// Circuit Breaker is Learning
+	Learning State = iota
+	// Circuit Breaker has learned
+	Learned
+)
+
+func (s State) String() string {
+	switch s {
+	case Learning:
+		return "Learning"
+	case Learned:
+		fallthrough
+	default:
+		return "Learned"
 	}
+}
 
+// A Smart TripFunction Generator
+//
+// All circuit breakers obtained out of a generator
+// share their learning state, but the circuit breaker state
+// (error rates, event counts, etc.) is not shared
+type SmartTripper struct {
+	decisionWindow time.Duration
+	policies       Policies
+	state          State
+	rate           float64
+	mu             sync.Mutex
+}
+
+// Returns Policies initialised to default values
+func NewPolicies() Policies {
+	return defaults
+}
+
+// Create a SmartTripper based on the nominal QPS for your task
+//
+// "Nominal QPS" is the basis on which the SmartTripper configures its
+// decision-making parameters. A suitable value for this parameter would be
+// your median QPS. If your QPS varies a lot during operation, choosing this
+// value closer to max QPS will make the circuit breaker more prone to tripping
+// during low traffic periods and choosing a value closer to min QPS will make it
+// a slow to respond during high traffic periods.
+func NewSmartTripper(QPS int, p Policies) *SmartTripper {
+	if QPS <= 0 {
+		panic("smartcb.NewSmartTripper: QPS should be >= 1")
+	}
+	decisionWindow := time.Millisecond * time.Duration(float64(p.SamplesPerWindow)*1000.0/float64(QPS))
+
+	return &SmartTripper{decisionWindow: decisionWindow, policies: p}
+}
+
+// Returns the Learning/Learned state of the Smart Tripper
+func (t *SmartTripper) State() State {
+	return t.state
+}
+
+// Returns the error rate that has been learned by the Smart Tripper
+func (t *SmartTripper) LearnedRate() float64 {
+	t.mu.Lock()
+	defer t.mu.Unlock()
+
+	return t.rate
+}
+
+func (t *SmartTripper) tripFunc() circuit.TripFunc {
 	var initTime time.Time
-	var mu sync.Mutex
-	rate := 0.0
 
-	recordError := func(cb *circuit.Breaker) float64 {
-		mu.Lock()
-		defer mu.Unlock()
+	learningCycles := t.policies.LearningWindowX
+	relearningCycles := t.policies.ReLearningWindowX
+	maxFail := t.policies.MaxFail
+
+	recordError := func(cb *circuit.Breaker) (float64, float64) {
+		t.mu.Lock()
+		defer t.mu.Unlock()
+
 		cbr := cb.ErrorRate()
-		rate = (learningCycles*rate + cbr) / (learningCycles + 1)
-		return cbr
+		t.rate = (t.policies.EWMADecayFactor*t.rate + cbr) / (t.policies.EWMADecayFactor + 1)
+
+		return t.rate, cbr
 	}
 
 	tripper := func(cb *circuit.Breaker) bool {
 		tElapsed := time.Since(initTime)
 
 		// Initiate Learning Phase
-		if initTime == (time.Time{}) || tElapsed > decisionWindow*relearningCycles {
+		if initTime == (time.Time{}) || tElapsed > t.decisionWindow*time.Duration(relearningCycles) {
 			initTime = time.Now()
 			tElapsed = time.Since(initTime)
+			t.state = Learning
 		}
 
 		// Learning
-		cycles := float64(tElapsed) / float64(decisionWindow)
+		cycles := float64(tElapsed) / float64(t.decisionWindow)
 		if cycles < learningCycles {
-			mu.Lock()
-			// Trip rate starts with maxFail and approaches the learned rate as learning nears completion
-			tripRate := 5*rate*cycles/float64(learningCycles) + maxFail*(float64(learningCycles)-cycles)/float64(learningCycles)
-			mu.Unlock()
-			return tripRate < recordError(cb)
+			lRate, eRate := recordError(cb)
+
+			// Trip t.rate starts with t.policies.MaxFail and approaches the Learned Rate * FailMultiplier as learning nears completion
+			lRateMultiplier := t.policies.FailMultiplier * cycles / learningCycles
+			maxFailMultiplier := (learningCycles - cycles) / learningCycles
+			tripRate := lRate*lRateMultiplier + maxFail*maxFailMultiplier
+			tripRate = min(tripRate, maxFail)
+
+			return tripRate < eRate && cb.Failures()+cb.Successes() > t.policies.SamplesPerWindow
 		}
-		mu.Lock()
-		defer mu.Unlock()
-		return 5*rate < cb.ErrorRate() && cb.Failures()+cb.Successes() > 10
+
+		t.state = Learned
+		return min(t.policies.FailMultiplier*t.LearnedRate(), maxFail) < cb.ErrorRate() && cb.Failures()+cb.Successes() > t.policies.SamplesPerWindow
 	}
 
-	options.ShouldTrip = tripper
+	return tripper
+}
+
+// Create a new circuit.Breaker using the dynamically self-configuring SmartTripper
+func NewSmartCircuitBreaker(t *SmartTripper) *circuit.Breaker {
+	options := &circuit.Options{
+		WindowTime: t.decisionWindow,
+	}
+	options.ShouldTrip = t.tripFunc()
 	return circuit.NewBreakerWithOptions(options)
 }
--- smartcb_test.go
+++ smartcb_test.go
@@ -2,14 +2,56 @@ package smartcb_test
 
 import (
 	"errors"
+	"log"
 	"math/rand"
 	"testing"
 	"time"
 
 	"github.com/codemartial/smartcb"
+	"github.com/rubyist/circuitbreaker"
 )
 
-var scb = smartcb.New(10000)
+func Example() {
+	// Initialise policies and set max. tolerable failure rate
+	// to 15% (= 0.15)
+	policies := smartcb.NewPolicies()
+	policies.MaxFail = 0.15
+
+	// Create a SmartTripper Generator for a 10k QPS task
+	var st = smartcb.NewSmartTripper(10000, policies)
+	// Create a Circuit Breaker from the SmartTripper Generator
+	var scb = smartcb.NewSmartCircuitBreaker(st)
+
+	// The task to be wrapped with the circuit breaker
+	protectedTask := func(errRate float64) (err error) {
+		if rand.Float64() < errRate {
+			return errors.New("forced error")
+		}
+		return nil
+	}
+
+	breakerEvents := scb.Subscribe()
+
+	// Let's run the example for 200 ms
+	stop := time.After(time.Millisecond * 200)
+	loop := true
+	for loop {
+		select {
+		case <-stop: // Stop execution now
+			loop = false
+		case e := <-breakerEvents: // Something changed with the circuit breaker
+			if e == circuit.BreakerTripped {
+				log.Println("Circuit Breaker tripped.", scb.ErrorRate(), st.State(), st.LearnedRate())
+				return
+			}
+		default: // Execute the task using circuit.Breaker.Call() method
+			_ = scb.Call(func() error { return protectedTask(0.02) }, time.Second)
+		}
+	}
+}
+
+var st = smartcb.NewSmartTripper(1000, smartcb.NewPolicies())
+var scb = smartcb.NewSmartCircuitBreaker(st)
 
 func protectedTask(errRate float64) (err error) {
 	if rand.Float64() < errRate {
@@ -20,23 +62,28 @@ func protectedTask(errRate float64) (err error) {
 
 func TestSmartCBLearning(t *testing.T) {
 	testStop := time.After(time.Millisecond * 1100)
+	bEvents := scb.Subscribe()
 	loop := true
 	for loop {
 		select {
 		case <-testStop:
 			loop = false
-		default:
-			err := scb.Call(func() error { return protectedTask(0.02) }, time.Second)
-			if err != nil && scb.Tripped() {
-				t.Error("Circuit Breaker tripped in Learning Phase. Error Rate:", scb.ErrorRate())
+		case e := <-bEvents:
+			if e == circuit.BreakerTripped {
+				t.Error("Circuit Breaker tripped in Learning Phase.", scb.ErrorRate(), st.State(), st.LearnedRate())
 				return
 			}
+		default:
+			_ = scb.Call(func() error { return protectedTask(0.02) }, time.Second)
 		}
 	}
 }
 
 func TestSmartCBOperationFail(t *testing.T) {
 	scb.ResetCounters()
+	if st.State() != smartcb.Learned {
+		t.Error("Circuit Breaker is still learning")
+	}
 	for i := 0; i < 1000; i++ {
 		err := scb.Call(func() error { return protectedTask(0.2) }, time.Second)
 		if err != nil && scb.Tripped() {
@@ -44,14 +91,13 @@ func TestSmartCBOperationFail(t *testing.T) {
 		}
 	}
 	if !scb.Tripped() {
-		t.Error("Circuit Breaker did not trip beyond learned failure rate", scb.ErrorRate())
+		t.Error("Circuit Breaker did not trip beyond learned failure rate", scb.ErrorRate(), st.LearnedRate())
 	}
 }
 
 func TestCBOperationSuccess(t *testing.T) {
 	scb.Reset()
-	var i = 0
-	for ; ; i++ {
+	for {
 		_ = scb.Call(func() error { return protectedTask(0) }, time.Second)
 		if scb.Ready() && scb.Successes() > 50 {
 			break
@@ -65,6 +111,7 @@ func TestCBOperationSuccess(t *testing.T) {
 		}
 	}
 }
+
 func TestInvalidDuration(t *testing.T) {
 	defer func() {
 		if recover() == nil {
@@ -72,11 +119,11 @@ func TestInvalidDuration(t *testing.T) {
 		}
 	}()
 
-	_ = smartcb.New(0)
+	_ = smartcb.NewSmartTripper(0, smartcb.NewPolicies())
 }
 
-func TestOverFailLearnGuard(t *testing.T) {
-	scb = smartcb.New(10000)
+func TestLearnGuard(t *testing.T) {
+	scb = smartcb.NewSmartCircuitBreaker(smartcb.NewSmartTripper(1000, smartcb.NewPolicies()))
 	loop := true
 	testStop := time.After(time.Millisecond * 1100)
 	var tripped bool
@@ -93,42 +140,50 @@ func TestOverFailLearnGuard(t *testing.T) {
 	if !tripped {
 		t.Error("Circuit Breaker learned a dangerous failure rate ", scb.ErrorRate())
 	}
-	t.Log(scb.ErrorRate())
 }
 
 func TestLongRun(t *testing.T) {
-	scb = smartcb.New(10000)
-	ticker := time.Tick(time.Microsecond * 100)
+	if testing.Short() {
+		return
+	}
+	st = smartcb.NewSmartTripper(1000, smartcb.NewPolicies())
+	scb = smartcb.NewSmartCircuitBreaker(st)
+	ticker := time.Tick(time.Millisecond)
 	testStop := time.After(time.Second * 2)
-	prevState := false
 	loop := true
-	start := time.Now()
+	bEvents := scb.Subscribe()
 	for loop {
 		select {
 		case <-testStop:
 			loop = false
+		case e := <-bEvents:
+			if e == circuit.BreakerTripped {
+				loop = false
+				t.Error("Circuit Breaker tripped in Learning Phase.", scb.ErrorRate(), st.State(), st.LearnedRate())
+			}
 		default:
 			_ = scb.Call(func() error { <-ticker; return protectedTask(0.02) }, time.Second)
-			if scb.Tripped() != prevState {
-				prevState = !prevState
-				t.Log(time.Since(start), prevState, scb.ErrorRate())
-			}
 		}
 	}
 
+	tripped := false
 	testStop = time.After(time.Second * 1)
 	loop = true
 	for loop {
 		select {
 		case <-testStop:
 			loop = false
+		case e := <-bEvents:
+			if e == circuit.BreakerTripped {
+				tripped = true
+			}
+			//t.Log(time.Since(start), e, scb.ErrorRate())
 		default:
 			_ = scb.Call(func() error { <-ticker; return protectedTask(0.2) }, time.Second)
-			if scb.Tripped() != prevState {
-				prevState = !prevState
-				t.Log(time.Since(start), prevState, scb.ErrorRate())
-			}
 		}
+	}
+	if !tripped {
+		t.Error("Circuit Breaker didn't trip above learned rate", scb.ErrorRate(), st.LearnedRate())
 	}
 
 	testStop = time.After(time.Second * 2)
@@ -139,10 +194,17 @@ func TestLongRun(t *testing.T) {
 			loop = false
 		default:
 			_ = scb.Call(func() error { <-ticker; return protectedTask(0.02) }, time.Second)
-			if scb.Tripped() != prevState {
-				prevState = !prevState
-				t.Log(time.Since(start), prevState, scb.ErrorRate())
-			}
 		}
+	}
+}
+
+func TestStateLabels(t *testing.T) {
+	tests := map[smartcb.State]string{smartcb.Learning: "Learning", smartcb.Learned: "Learned"}
+	for k, v := range tests {
+		t.Run(v, func(t *testing.T) {
+			if k.String() != v {
+				t.Error("Invalid Label for Learning State. Expected ", v, ", got ", smartcb.Learning.String())
+			}
+		})
 	}
 }
